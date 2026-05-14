5c317d2f4e513284a8aa48ed48afb0724e510570
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-13 23:04:00 -0400

    Eliminar yotsuba.js

--- src/db/yotsuba.js
+++
@@ -1,8 +0,0 @@
-export default {
-    id: "005",
-    name: "Yotsuba Nakano",
-    source: "Gotoubun no Hanayome",
-    rarity: "SR",
-    description: "La cuarta hermana de las quintillizas Nakano, conocida por su energía y su cinta con forma de orejas de conejo.",
-    file: "yotsuba_nakano.jpg"
-}
