5040d5ebc5b073f5aab9b4ebdee16bbbdf3410ca
Félix Ofx <xlfelixgamer@gmail.com>
2026-05-13 22:20:38 -0400

    Actualizar index.js

--- index.js
+++ index.js
@@ -3,12 +3,25 @@ import { join, dirname } from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
-const imagesPath = join(__dirname, './img');
+const dbPath = join(__dirname, './src/db');
+const imgPath = join(__dirname, './img');
 
-export const getGachaImage = () => {
-    const files = fs.readdirSync(imagesPath);
+export const getFullGacha = async () => {
+    const files = fs.readdirSync(dbPath).filter(f => f.endsWith('.js'));
     const randomFile = files[Math.floor(Math.random() * files.length)];
-    return join(imagesPath, randomFile);
+    
+    const { default: data } = await import(`./src/db/${randomFile}`);
+    
+    return {
+        ...data,
+        fullPath: join(imgPath, data.file)
+    };
 };
 
-export const allImages = fs.readdirSync(imagesPath);
+export const getAllData = async () => {
+    const files = fs.readdirSync(dbPath).filter(f => f.endsWith('.js'));
+    return Promise.all(files.map(async (f) => {
+        const { default: data } = await import(`./src/db/${f}`);
+        return data;
+    }));
+};
